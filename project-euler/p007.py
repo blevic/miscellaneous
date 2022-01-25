8472df396493eaedb18c3aa2788f0515dc03caad
brenolevi <brenolevi@ymail.com>
2022-01-25 20:51:51 -0300

    Add much faster primality check

--- project-euler/p007.py
+++ project-euler/p007.py
@@ -1,6 +1,8 @@
 # What is the 10 001st prime number?
+from math import sqrt
 
-def nth_prime(n):
+
+def very_slow_and_awful_nth_prime(n):
     memory = [2]
     counter = 1
     candidate = 3
@@ -17,5 +19,36 @@ def nth_prime(n):
     return memory[-1]
 
 
+def is_prime(n):
+    if n == 2 or n == 3:
+        return True
+    elif n < 2 or n % 2 == 0:
+        return False
+    elif n < 9:
+        return True
+    elif n % 3 == 0:
+        return False
+
+    r = int(sqrt(n))
+    f = 5
+
+    while f <= r:
+        if n % f == 0 or n % (f + 2) == 0:
+            return False
+        else:
+            f += 6
+    return True
+
+
+def fast_nth_prime(n):
+    count = 1
+    candidate = 1
+    while count < n:
+        candidate += 2
+        if is_prime(candidate):
+            count += 1
+    return candidate
+
+
 number = 10001
-print(nth_prime(number))
+print(fast_nth_prime(number))
