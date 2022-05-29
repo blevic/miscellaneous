a27501714dbf5b0b6175e43f29eadce27d01d42a
brenolevi <brenolevi@ymail.com>
2022-05-29 14:56:24 -0300

    Implement drawing function

--- rubik-cube-simple-solver/RubikCube.py
+++ rubik-cube-simple-solver/RubikCube.py
@@ -4,6 +4,7 @@ from RubikCubeInterface import RubikCubeInterface
 
 CUBE_SIZE = 3
 
+
 class Color(Enum):
     """Colors enumeration"""
     RED = 1
@@ -12,6 +13,7 @@ class Color(Enum):
     WHITE = 4
     BLUE = 5
     GREEN = 6
+    BLACK = 7
 
 
 class RubikCube(RubikCubeInterface):
@@ -27,13 +29,46 @@ class RubikCube(RubikCubeInterface):
 
     def draw(self) -> None:
         """Overrides RubikCubeInterface.draw()"""
-        print(self.faceFront)
-        print(self.faceBack)
-        print(self.faceUp)
-        print(self.faceDown)
-        print(self.faceLeft)
-        print(self.faceRight)
+        def emoji(color):
+            emoji_map = {
+                Color.RED:    '🟥',
+                Color.ORANGE: '🟧',
+                Color.YELLOW: '🟨',
+                Color.WHITE:  '⬜',
+                Color.BLUE:   '🟦',
+                Color.GREEN:  '🟩',
+                Color.BLACK:  '⬛'
+            }
 
+            return emoji_map[color]
+
+        padding = emoji(Color.BLACK)
+
+        for row in range(self.size):
+            upper_lines = (padding + " ") * self.size
+            for square in range(self.size):
+                upper_lines += emoji(self.faceUp[row][square]) + " "
+            upper_lines += (padding + " ") * 2 * self.size
+            print(upper_lines)
+
+        for row in range(self.size):
+            middle_lines = ""
+            for square in range(self.size):
+                middle_lines += emoji(self.faceLeft[row][square]) + " "
+            for square in range(self.size):
+                middle_lines += emoji(self.faceFront[row][square]) + " "
+            for square in range(self.size):
+                middle_lines += emoji(self.faceRight[row][square]) + " "
+            for square in range(self.size):
+                middle_lines += emoji(self.faceBack[row][square]) + " "
+            print(middle_lines)
+
+        for row in range(self.size):
+            lower_lines = (padding + " ") * self.size
+            for square in range(self.size):
+                lower_lines += emoji(self.faceDown[row][square]) + " "
+            lower_lines += (padding + " ") * 2 * self.size
+            print(lower_lines)
 
     def __create_face(self, color):
         return [[color] * self.size] * self.size
