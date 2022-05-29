738a6f51e3d6662eae95cd3c41eafa712419c216
brenolevi <brenolevi@ymail.com>
2022-05-29 16:41:01 -0300

    Declare move method

--- rubik-cube-simple-solver/RubikCubeInterface.py
+++ rubik-cube-simple-solver/RubikCubeInterface.py
@@ -3,6 +3,25 @@ class RubikCubeInterface:
         """Draw the cube"""
         pass
 
+    def move(self, turn: str) -> None:
+        """
+        Move the cube
+
+            Args:
+                turn: a letter denoting the movement/turn to be executed, according to Singmaster notation
+                    Allowed letters:
+                        F: Front;  B: Back;  U: Up;  D: Down;  L: Left;  R: Right;
+                        f: Front 2 layers;  b: Back 2 layers;
+                        u: Up 2 layers;  d: Down 2 layers;
+                        l: Left 2 layers;  r: Right 2 layers;
+                        x: rotation;  y: rotation;  z: rotation;
+                        M: Middle;  E: Equator;  S: Standing
+
+                    If followed by prime symbol (′), turn it anticlockwise; otherwise, turn it clockwise.
+                    If followed by a 2, execute the operation twice.
+        """
+        pass
+
     def shuffle(self, steps: int) -> None:
         """Shuffle the cube"""
         pass
