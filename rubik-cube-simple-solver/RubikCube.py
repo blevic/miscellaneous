4acf1ab7f5b7e24ccc0b676f161b5ceeb2d1e083
brenolevi <brenolevi@ymail.com>
2022-05-29 20:17:33 -0300

    Implement __move_F function

--- rubik-cube-simple-solver/RubikCube.py
+++ rubik-cube-simple-solver/RubikCube.py
@@ -18,6 +18,7 @@ class Color(Enum):
 
 class RubikCube(RubikCubeInterface):
     """Models Rubik's Cube"""
+
     def __init__(self):
         self.size = CUBE_SIZE
         self.faceFront = self.__create_face(Color.RED)
@@ -29,15 +30,16 @@ class RubikCube(RubikCubeInterface):
 
     def draw(self) -> None:
         """Overrides RubikCubeInterface.draw()"""
+
         def emoji(color):
             emoji_map = {
-                Color.RED:    '🟥',
+                Color.RED: '🟥',
                 Color.ORANGE: '🟧',
                 Color.YELLOW: '🟨',
-                Color.WHITE:  '⬜',
-                Color.BLUE:   '🟦',
-                Color.GREEN:  '🟩',
-                Color.BLACK:  '⬛'
+                Color.WHITE: '⬜',
+                Color.BLUE: '🟦',
+                Color.GREEN: '🟩',
+                Color.BLACK: '⬛'
             }
 
             return emoji_map[color]
@@ -64,14 +66,117 @@ class RubikCube(RubikCubeInterface):
                           + (pad + " ") * 2 * n
             print(lower_lines)
 
+    def move(self, turn: str) -> None:
+        """Overrides RubikCubeInterface.move(turn)"""
+        turn_functions = {
+            'F': self.__move_F,
+            'B': self.__move_B,
+            'U': self.__move_U,
+            'D': self.__move_D,
+            'L': self.__move_L,
+            'R': self.__move_R,
+            'f': self.__move_f,
+            'b': self.__move_b,
+            'u': self.__move_u,
+            'd': self.__move_d,
+            'l': self.__move_l,
+            'r': self.__move_r,
+            'x': self.__move_x,
+            'y': self.__move_y,
+            'z': self.__move_z,
+            'M': self.__move_M,
+            'E': self.__move_E,
+            'S': self.__move_S
+        }
+
+        turn_functions[turn]()
+
+    @staticmethod
+    def __rotate_clockwise(face: list) -> list:
+        return [[face[j][i] for j in range(len(face[0]) - 1, -1, -1)] for i in range(len(face))]
+
+    @staticmethod
+    def __rotate_counterclockwise(face: list) -> list:
+        return [[face[j][i] for j in range(len(face))] for i in range(len(face[0]) - 1, -1, -1)]
+
+    def __move_F(self) -> None:
+        self.faceFront = self.__rotate_clockwise(self.faceFront)
+
+        n = self.size
+
+        _saved_up = self.faceUp[n - 1]
+        _saved_down = self.faceDown[0]
+        _saved_left = [self.faceLeft[row][n - 1] for row in range(n)]
+        _saved_right = [self.faceRight[row][0] for row in range(n)]
+
+        self.faceUp[n - 1] = _saved_left[::-1]
+        self.faceDown[0] = _saved_right[::-1]
+
+        for i in range(n):
+            self.faceLeft[i][n - 1] = _saved_down[i]
+
+        for i in range(n):
+            self.faceRight[i][0] = _saved_up[i]
+
+    def __move_B(self) -> None:
+        pass
+
+    def __move_U(self) -> None:
+        pass
+
+    def __move_D(self) -> None:
+        pass
+
+    def __move_L(self) -> None:
+        pass
+
+    def __move_R(self) -> None:
+        pass
+
+    def __move_f(self) -> None:
+        pass
+
+    def __move_b(self) -> None:
+        pass
+
+    def __move_u(self) -> None:
+        pass
+
+    def __move_d(self) -> None:
+        pass
+
+    def __move_l(self) -> None:
+        pass
+
+    def __move_r(self) -> None:
+        pass
+
+    def __move_x(self) -> None:
+        pass
+
+    def __move_y(self) -> None:
+        pass
+
+    def __move_z(self) -> None:
+        pass
+
+    def __move_M(self) -> None:
+        pass
+
+    def __move_E(self) -> None:
+        pass
+
+    def __move_S(self) -> None:
+        pass
+
     def is_solved(self) -> bool:
         """Overrides RubikCubeInterface.is_solved()"""
         return len(set([s for row in self.faceFront for s in row])) == 1 \
-               and len(set([s for row in self.faceBack for s in row])) == 1 \
-               and len(set([s for row in self.faceUp for s in row])) == 1 \
-               and len(set([s for row in self.faceDown for s in row])) == 1 \
-               and len(set([s for row in self.faceLeft for s in row])) == 1 \
-               and len(set([s for row in self.faceRight for s in row])) == 1
+            and len(set([s for row in self.faceBack for s in row])) == 1 \
+            and len(set([s for row in self.faceUp for s in row])) == 1 \
+            and len(set([s for row in self.faceDown for s in row])) == 1 \
+            and len(set([s for row in self.faceLeft for s in row])) == 1 \
+            and len(set([s for row in self.faceRight for s in row])) == 1
 
     def __create_face(self, color):
         return [[color] * self.size] * self.size
