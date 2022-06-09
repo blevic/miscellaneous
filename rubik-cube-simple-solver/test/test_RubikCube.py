c410a8a931266957023f52c878ddcc04615e73fa
brenolevi <brenolevi@ymail.com>
2022-06-09 00:03:47 -0300

    Create test_invalid_moves()

--- rubik-cube-simple-solver/test/test_RubikCube.py
+++ rubik-cube-simple-solver/test/test_RubikCube.py
@@ -13,6 +13,14 @@ class TestRubikCube(unittest.TestCase):
             except ValueError:
                 self.fail("move() raised ValueError unexpectedly!")
 
+    def test_invalid_moves(self):
+        invalid_moves = ["F'", "A", "m", "Fpp", "pF", "S22", "S3" "FfuUY", "F ", "F2′"]
+        cube = RubikCube()
+
+        for moves in invalid_moves:
+            with self.assertRaises(ValueError):
+                cube.move(moves)
+
 
 if __name__ == '__main__':
     unittest.main()
